efcb3b6dd5043ece981f682d46f1159386d0247c
Ethan Domingo <ethanddomingo@gmail.com>
2019-11-29 22:56:51 -0800

    Adding Assignment #5

---
+++ Assignment_5/stat.py
@@ -0,0 +1,44 @@
+import unittest
+
+lis = []
+
+def getMean():
+    pass
+    '''
+    take in entire list along with counter and then divide by the counter
+    may take a while but this will be the initial test
+    '''
+
+def getMode():
+    '''
+    maybe we can use split??
+    alternative is to just mark one number and see which is the greatest
+    keep taking out numbers until one has taken out the most
+    '''
+    pass
+
+def getMedian():
+    '''
+    order the entire list
+    then find the middle
+    if the middle is even, then add two numbers together then divide by two
+    '''
+    pass
+
+def getMin():
+    '''
+    divide and conquer
+    '''
+    pass
+
+def getMax():
+    '''
+    divide and conquer
+    '''
+    pass
+
+def main():
+    lis = input().split()
+
+if __name__ == '__main__':
+    main()
